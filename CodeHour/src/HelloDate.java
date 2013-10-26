a852be45234030699cc0b2d89258945513255dd2
pearlytan <pearlytan@10.24.146.71>
2013-10-25 19:50:25 -0700

    HelloDate and HelloWorld

---
+++ CodeHour/src/HelloDate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class HelloDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Hello, it's: ");
+		System.out.println(new Date());
+	}
+
+}
