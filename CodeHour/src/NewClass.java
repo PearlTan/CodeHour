8ec1f94097953ebdb4a58987a8b485ac230c4969
chiaming2000 <chiaming2000@gmail.com>
2013-10-25 18:31:19 -0700

    added new class

---
+++ CodeHour/src/NewClass.java
@@ -0,0 +1,13 @@
+
+public class NewClass {
+
+	public NewClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
